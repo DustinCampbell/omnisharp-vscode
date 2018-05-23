42bac470d482881ad95933106966944816e50df2
Dustin Campbell <dustin@teamcampbell.org>
2018-05-23 10:09:54 -0700

    Move 'toDocumentSymbol' to 'documentSymbolProvider'

--- src/features/documentSymbolProvider.ts
+++ src/features/documentSymbolProvider.ts
@@ -4,9 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 import AbstractSupport from './abstractProvider';
+import * as protocol from '../omnisharp/protocol';
 import * as serverUtils from '../omnisharp/utils';
-import {toDocumentSymbol} from '../omnisharp/typeConvertion';
-import {DocumentSymbolProvider, SymbolInformation, TextDocument, CancellationToken} from 'vscode';
+import {toRange} from '../omnisharp/typeConvertion';
+import {DocumentSymbolProvider, SymbolInformation, SymbolKind, TextDocument, CancellationToken} from 'vscode';
 
 export default class OmnisharpDocumentSymbolProvider extends AbstractSupport implements DocumentSymbolProvider {
 
@@ -21,3 +22,41 @@ export default class OmnisharpDocumentSymbolProvider extends AbstractSupport imp
         });
     }
 }
+
+
+export function toDocumentSymbol(bucket: SymbolInformation[], node: protocol.Node, containerLabel?: string): void {
+
+    let ret = new SymbolInformation(node.Location.Text, _kinds[node.Kind],
+        toRange(node.Location),
+        undefined, containerLabel);
+
+    if (node.ChildNodes) {
+        for (let child of node.ChildNodes) {
+            toDocumentSymbol(bucket, child, ret.name);
+        }
+    }
+    
+    bucket.push(ret);
+}
+
+const _kinds: { [kind: string]: SymbolKind; } = Object.create(null);
+
+// types
+_kinds['ClassDeclaration'] = SymbolKind.Class;
+_kinds['EnumDeclaration'] = SymbolKind.Enum;
+_kinds['InterfaceDeclaration'] = SymbolKind.Interface;
+_kinds['StructDeclaration'] = SymbolKind.Enum;
+
+// variables
+_kinds['VariableDeclaration'] = SymbolKind.Variable;
+
+// members
+_kinds['EnumMemberDeclaration'] = SymbolKind.Property;
+_kinds['EventFieldDeclaration'] = SymbolKind.Event;
+_kinds['FieldDeclaration'] = SymbolKind.Field;
+_kinds['MethodDeclaration'] = SymbolKind.Method;
+_kinds['PropertyDeclaration'] = SymbolKind.Property;
+
+// other stuff
+_kinds['NamespaceDeclaration'] = SymbolKind.Namespace;
+
